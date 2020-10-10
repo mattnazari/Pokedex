38f28f619d0bec8486cfb39c123fafe4e07fbbb3
mattnazari <matthew.nazari8@gmail.com>
2020-10-09 23:37:15 -0700

    Pokemon-Cards: pokemon names are showing

--- src/components/Pokemon/Card/index.js
+++ src/components/Pokemon/Card/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Card extends Component {
+  state = {
+    name: '',
+    imgUrl: '',
+    pokemonIndex: '',
+  };
+
   render() {
+    const { name, url } = this.props;
+
     return (
       <div className='col-md-3 col-sm-6 mb-5'>
         <div className='card'>
           <div className='cardHeader'>
-            <h1>Matt</h1>
+            <h1>{name}</h1>
           </div>
         </div>
       </div>
--- src/components/Pokemon/List/index.js
+++ src/components/Pokemon/List/index.js
@@ -5,7 +5,7 @@ import Card from '../Card';
 
 export default class List extends Component {
   state = {
-    url: 'https://pokeapi.co/api/v2/pokemon/',
+    url: 'https://pokeapi.co/api/v2/pokemon?limit=893&offset=0',
     pokemon: null,
   };
 
@@ -20,7 +20,7 @@ export default class List extends Component {
         {this.state.pokemon ? (
           <div className='row'>
             {this.state.pokemon.map((pokemon) => (
-              <Card />
+              <Card key={pokemon.name} name={pokemon.name} url={pokemon.url} />
             ))}
           </div>
         ) : (
